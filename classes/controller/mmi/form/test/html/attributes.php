c64f86533e377e3ce09c378091c78ce20e56e974
Shawn Sprehe <memakeit@gmail.com>
2010-09-10 10:02:25 -0600

    added html attribute and event classes

---
+++ classes/controller/mmi/form/test/html/attributes.php
@@ -0,0 +1,28 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Test controller for HTML attributes.
+ *
+ * @package		MMI Form
+ * @author		Me Make It
+ * @copyright	(c) 2010 Me Make It
+ * @license		http://www.memakeit.com/license
+ */
+class Controller_MMI_Form_Test_HTML_Attributes extends Controller
+{
+	/**
+	 * @var boolean turn debugging on?
+	 **/
+	public $debug = TRUE;
+
+	/**
+	 * Test the HTML attributes.
+	 *
+	 * @return	void
+	 */
+	public function action_index()
+	{
+		$data = MMI_HTML4_Attributes::get();
+		MMI_Debug::dump($data, 'html4 attributes');
+		$data = MMI_HTML5_Attributes::get();
+		MMI_Debug::dump($data, 'html5 attributes');	}
+} // End Controller_MMI_Form_Test_HTML_Attributes
