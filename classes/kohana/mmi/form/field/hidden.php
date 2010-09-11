ffa931902130ca089fce43b9749ba83b9740f4d8
Shawn Sprehe <memakeit@gmail.com>
2010-09-11 14:11:14 -0600

    adding file, hidden, password, reset, submit, and text fields

---
+++ classes/kohana/mmi/form/field/hidden.php
@@ -0,0 +1,33 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Hidden field.
+ *
+ * @package		MMI Form
+ * @author		Me Make It
+ * @copyright	(c) 2010 Me Make It
+ * @license		http://www.memakeit.com/license
+ */
+class Kohana_MMI_Form_Field_Hidden extends MMI_Form_Field
+{
+	/**
+	 * Set the field type.
+	 *
+	 * @param	array	an associative array of field options
+	 * @return	void
+	 */
+	public function __construct($options = array())
+	{
+		if ( ! is_array($options))
+		{
+			$options = array();
+		}
+
+		$options['type'] = 'hidden';
+		$order = Arr::get($options, '_order');
+		if (empty($order))
+		{
+			$options['_order'] = array(MMI_Form::ORDER_FIELD);
+		}
+		parent::__construct($options);
+	}
+} // End Kohana_MMI_Form_Field_Hidden
---
+++ classes/kohana/mmi/form/field/password.php
@@ -0,0 +1,27 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Password field.
+ *
+ * @package		MMI Form
+ * @author		Me Make It
+ * @copyright	(c) 2010 Me Make It
+ * @license		http://www.memakeit.com/license
+ */
+class Kohana_MMI_Form_Field_Password extends MMI_Form_Field
+{
+	/**
+	 * Set the field type.
+	 *
+	 * @param	array	an associative array of field options
+	 * @return	void
+	 */
+	public function __construct($options = array())
+	{
+		if ( ! is_array($options))
+		{
+			$options = array();
+		}
+		$options['type'] = 'password';
+		parent::__construct($options);
+	}
+} // End Kohana_MMI_Form_Field_Password
