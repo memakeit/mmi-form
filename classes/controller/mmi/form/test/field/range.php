082f511da95ec2358510cf699ee31acd43d099a0
Shawn Sprehe <memakeit@gmail.com>
2010-09-30 10:02:32 -0600

    added min, max and step server-side validation for number and range inputs

--- classes/controller/mmi/form/test/field/range.php
+++ classes/controller/mmi/form/test/field/range.php
@@ -20,33 +20,50 @@ class Controller_MMI_Form_Test_Field_Range extends Controller_MMI_Form_Test_Fiel
 
 		$settings = array
 		(
-			'_after' => '100',
-			'_before' => '0',
+			'_after' => 100,
+			'_before' => 0,
 			'_label' => 'Range 1',
 			'_namespace' => 'mmi',
 			'class' => 'range',
 			'id' => 'range1',
 			'max' => 100,
 			'min' => 0,
+			'required' => 'required',
 			'step' => 10,
 			'value' => 20,
 		);
 		$field = MMI_Form_Field::factory($type, $settings);
 		$this->_form->add_field($field);
-		MMI_Debug::dump($field->render(), $type);
+		MMI_Debug::dump($field->render(), $type.' (min 0; max 100; step 10)');
 
 		$settings = array_merge($settings, array
 		(
-			'_after' => '10',
+			'_before' => 0,
 			'_label' => 'Range 2',
 			'id' => 'range2',
-			'max' => 10,
 			'min' => 0,
-			'step' => 1,
+			'required' => FALSE,
+			'step' => 2,
 			'value' => 6,
 		));
+		unset($settings['_after'], $settings['max']);
 		$field = MMI_Form_Field::factory($type, $settings);
 		$this->_form->add_field($field);
-		MMI_Debug::dump($field->render(), $type);
+		MMI_Debug::dump($field->render(), $type. ' (min 0; step 2)');
+
+		$settings = array_merge($settings, array
+		(
+			'_after' => 100,
+			'_label' => 'Range 3',
+			'id' => 'range3',
+			'max' => 100,
+			'required' => FALSE,
+			'step' => 25,
+			'value' => 6,
+		));
+		unset($settings['_before'], $settings['min']);
+		$field = MMI_Form_Field::factory($type, $settings);
+		$this->_form->add_field($field);
+		MMI_Debug::dump($field->render(), $type. ' (max 100; step 25)');
 	}
 } // End Controller_MMI_Form_Test_Field_Range
---
+++ classes/kohana/mmi/form/rule/minmaxstep/numeric.php
@@ -0,0 +1,149 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Validation rules to enforce numeric min, max, and step attributes.
+ *
+ * @package		MMI Form
+ * @author		Me Make It
+ * @copyright	(c) 2010 Me Make It
+ * @license		http://www.memakeit.com/license
+ */
+class Kohana_MMI_Form_Rule_MinMaxStep_Numeric
+{
+	/**
+	 * Create the min, max, and step rules.
+	 *
+	 * @param	MMI_Form_Field	a form field object
+	 * @return	void
+	 */
+	public static function init(MMI_Form_Field $field)
+	{
+		$attributes = $field->attribute();
+		$meta = $field->meta();
+		$callbacks = Arr::get($meta, 'callbacks', array());
+		if ( ! is_array($callbacks))
+		{
+			$callbacks = array();
+		}
+		$rules = Arr::get($meta, 'rules', array());
+		if ( ! is_array($rules))
+		{
+			$rules = array();
+		}
+
+		// Process max and min attributes
+		$min = Arr::get($attributes, 'min');
+		$max = Arr::get($attributes, 'max');
+		if (is_numeric($min) AND is_numeric($max))
+		{
+			$step = Arr::get($attributes, 'step');
+			$rules['range'] = array($min, $max);
+		}
+		elseif (is_numeric($min))
+		{
+			$step = Arr::get($attributes, 'step');
+			$callbacks['min_numeric'] = array
+			(
+				array('MMI_Form_Rule_MinMaxStep_Numeric', 'min'), array('min' => $min)
+			);
+		}
+		elseif (is_numeric($max))
+		{
+			$step = Arr::get($attributes, 'step');
+			$callbacks['max_numeric'] = array
+			(
+				array('MMI_Form_Rule_MinMaxStep_Numeric', 'max'), array('max' => $max)
+			);
+		}
+
+		// Process step attribute
+		if (isset($step))
+		{
+			$callbacks['step_numeric'] = array
+			(
+				array('MMI_Form_Rule_MinMaxStep_Numeric', 'step'), array('max' => $max, 'min' => $min, 'step' => $step)
+			);
+		}
+		$field->meta('callbacks', $callbacks);
+		$field->meta('rules', $rules);
+	}
+
+	/**
+	 * Test if a number is less than or equal than a maximum value.
+	 *
+	 * @param 	Validate	the validation object
+	 * @param	string		the field name
+	 * @param	array		the validation parameters
+	 * @return	boolean
+	 */
+	public static function max(Validate $validate, $field, $parms = array())
+	{
+		$max = array_shift($parms);
+		$value = Arr::get($_POST, $field);
+		if (is_numeric($max) AND is_numeric($value))
+		{
+			if (intval($value) <= intval($max))
+			{
+				return TRUE;
+			}
+			$validate->error($field, 'max_numeric', array($max));
+		}
+		return FALSE;
+	}
+
+	/**
+	 * Test if a number is greater than or equal than a minimum value.
+	 *
+	 * @param 	Validate	the validation object
+	 * @param	string		the field name
+	 * @param	array		the validation parameters
+	 * @return	boolean
+	 */
+	public static function min(Validate $validate, $field, $parms = array())
+	{
+		$min = array_shift($parms);
+		$value = Arr::get($_POST, $field);
+		if (is_numeric($min) AND is_numeric($value))
+		{
+			if (intval($value) >= intval($min))
+			{
+				return TRUE;
+			}
+			$validate->error($field, 'min_numeric', array($min));
+		}
+		return FALSE;
+	}
+
+	/**
+	 * Test if a number is evenly divisible by the step value.
+	 *
+	 * @param 	Validate	the validation object
+	 * @param	string		the field name
+	 * @param	array		the validation parameters
+	 * @return	boolean
+	 */
+	public static function step(Validate $validate, $field, $parms = array())
+	{
+		$max = Arr::get($parms, 'max');
+		$min = Arr::get($parms, 'min');
+		$step = Arr::get($parms, 'step');
+		$value = Arr::get($_POST, $field);
+		if (is_numeric($step) AND is_numeric($value))
+		{
+			if (is_numeric(($min)) AND $value >= $min)
+			{
+				$value = $value - $min;
+			}
+			elseif (is_numeric(($max)) AND $value <= $max)
+			{
+				$value = $max - $value;
+			}
+			$div = $value / $step;
+			if (ceil($div) == $div)
+			{
+				return TRUE;
+			}
+			$validate->error($field, 'step_numeric', array($step));
+		}
+		return FALSE;
+	}
+} // End Kohana_MMI_Form_Rule_MinMaxStep_Numeric
