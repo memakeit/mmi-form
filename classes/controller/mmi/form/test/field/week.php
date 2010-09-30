6358263dd5050ca4f1bb4de78f88b0ac0c1a7b6d
Shawn Sprehe <memakeit@gmail.com>
2010-09-30 14:06:48 -0600

    added min, max and step server-side validation for number and range inputs

--- classes/controller/mmi/form/test/field/week.php
+++ classes/controller/mmi/form/test/field/week.php
@@ -20,28 +20,49 @@ class Controller_MMI_Form_Test_Field_Week extends Controller_MMI_Form_Test_Field
 
 		$settings = array
 		(
+			'_after' => '2010-W10',
+			'_before' => '2010-W01',
 			'_label' => 'Week 1',
 			'_namespace' => 'mmi',
 			'class' => 'week',
 			'id' => 'week1',
+			'max' => '2010-W10',
+			'min' => '2010-W01',
 			'required' => 'required',
+			'step' => 1,
 		);
 		$field = MMI_Form_Field::factory($type, $settings);
 		$this->_form->add_field($field);
-		MMI_Debug::dump($field->render(), $type);
+		MMI_Debug::dump($field->render(), $type.' (min 2010-W01; max 2010-W10; step 1)');
 
 		$settings = array_merge($settings, array
 		(
+			'_before' => '2010-W10',
 			'_label' => 'Week 2',
-			'_namespace' => 'mmi',
-			'class' => 'week',
 			'id' => 'week2',
+			'min' => '2010-W10',
 			'required' => FALSE,
 			'step' => 2,
-			'value' => '2010-W11',
+			'value' => '2010-W01',
 		));
+		unset($settings['_after'], $settings['max']);
 		$field = MMI_Form_Field::factory($type, $settings);
 		$this->_form->add_field($field);
-		MMI_Debug::dump($field->render(), $type.' (step 2)');
+		MMI_Debug::dump($field->render(), $type. ' (min 2010-W10; step 2)');
+
+		$settings = array_merge($settings, array
+		(
+			'_after' => '2010-W30',
+			'_label' => 'Week 3',
+			'id' => 'week3',
+			'max' => '2010-W30',
+			'required' => FALSE,
+			'step' => 3,
+			'value' => '2010-W11',
+		));
+		unset($settings['_before'], $settings['min']);
+		$field = MMI_Form_Field::factory($type, $settings);
+		$this->_form->add_field($field);
+		MMI_Debug::dump($field->render(), $type. ' (max 2010-W30; step 3)');
 	}
 } // End Controller_MMI_Form_Test_Field_Week
