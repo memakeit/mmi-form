3884ebfd6be6d695575a36a9a0a5c7450cf3a070
Shawn Sprehe <memakeit@gmail.com>
2010-10-01 11:30:14 -0600

    validation messages

---
+++ messages/en/validate.php
@@ -0,0 +1,30 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+// English
+return array
+(
+	'alpha'			=> 'Please enter only alphabetic characters.',
+	'alpha_dash'	=> 'Please enter only alphanumeric, dash, and underscore characters.',
+	'alpha_numeric'	=> 'Please enter only alphanumeric characters.',
+	'color'			=> 'Please enter a valid color.',
+	'credit_card'	=> 'Please enter a valid credit card number.',
+	'custom_max'	=> 'Please enter a value less than or equal to :param1.',
+	'custom_min'	=> 'Please enter a value greater than or equal to :param1.',
+	'custom_step'	=> 'Please enter a value that is a multiple of :param1 :param2.',
+	'date'			=> 'Please enter a valid date.',
+	'decimal'		=> 'Please enter a decimal with :param1 places.',
+	'digit'			=> 'Please enter only digits.',
+	'email'			=> 'Please enter a valid email address.',
+	'email_domain'	=> 'Please enter a valid email address.',
+	'exact_length'	=> 'Please enter exactly :param1 characters.',
+	'ip'			=> 'Please enter a valid IP address.',
+	'matches'		=> 'Please enter the same value again.',
+	'max_length'	=> 'Please enter at most :param1 characters.',
+	'min_length'	=> 'Please enter at least :param1 characters.',
+	'not_empty'		=> 'Required',
+	'numeric'		=> 'Please enter a valid number.',
+	'phone'			=> 'Please enter a valid phone number.',
+	'range'			=> 'Please enter a value between :param1 and :param2.',
+	'regex'			=> 'Please enter a valid format.',
+	'url'			=> 'Please enter a valid URL.',
+);
---
+++ messages/es/validate.php
@@ -0,0 +1,30 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+// Spanish
+return array
+(
+	'alpha'			=> 'Por favor, introduzca sólo caracteres alfabéticos',
+	'alpha_dash'	=> 'Por favor, introduzca sólo caracteres alfanuméricos, guiones y guiones bajos.',
+	'alpha_numeric'	=> 'Por favor, introduzca sólo caracteres alfanuméricos.',
+	'color'			=> 'Por favor, introduzca un color válido.',
+	'credit_card'	=> 'Por favor, introduzca un número de tarjeta de crédito válida.',
+	'custom_max'	=> 'Por favor, introduzca un valor menor o igual a :param1.',
+	'custom_min'	=> 'Por favor, introduzca un valor mayor o igual a :param1.',
+	'custom_step'	=> 'Por favor, introduzca un valor que es un múltiplo de :param1 :param2.',
+	'date'			=> 'Por favor, introduzca una fecha válida.',
+	'decimal'		=> 'Por favor, introduzca un número decimal con: param1 lugares.',
+	'digit'			=> 'Por favor, introduzca sólo números.',
+	'email'			=> 'Por favor, introduzca una dirección válida de correo electrónico.',
+	'email_domain'	=> 'Por favor, introduzca una dirección válida de correo electrónico.',
+	'exact_length'	=> 'Por favor, introduzca exactamente :param1 caracteres.',
+	'ip'			=> 'Por favor, introduzca una dirección IP válida.',
+	'matches'		=> 'Por favor, introduzca el mismo valor de nuevo.',
+	'max_length'	=> 'Por favor, introduce un máximo de: param1 caracteres.',
+	'min_length'	=> 'Por favor, introduzca al menos: param1 caracteres.',
+	'not_empty'		=> 'Necesario',
+	'numeric'		=> 'Por favor, introduzca un número válido.',
+	'phone'			=> 'Por favor, introduzca un número de teléfono válido.',
+	'range'			=> 'Por favor, introduzca un valor entre: param1 y: param2.',
+	'regex'			=> 'Por favor, introduzca un formato válido.',
+	'url'			=> 'Por favor, introduzca una URL válida.',
+);
