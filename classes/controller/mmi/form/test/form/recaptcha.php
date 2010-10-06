e9d7da7b18943ece5d17f7453dce22eb6f789437
Shawn Sprehe <memakeit@gmail.com>
2010-10-06 10:31:23 -0600

    added recaptcha test form

---
+++ classes/controller/mmi/form/test/form/recaptcha.php
@@ -0,0 +1,52 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Test controller for a form with the reCAPTCHA plugin.
+ *
+ * @package		MMI Form
+ * @author		Me Make It
+ * @copyright	(c) 2010 Me Make It
+ * @license		http://www.memakeit.com/license
+ */
+class Controller_MMI_Form_Test_Form_reCAPTCHA extends Controller
+{
+	/**
+	 * @var boolean turn debugging on?
+	 **/
+	public $debug = TRUE;
+
+	public function action_index()
+	{
+		$settings = array
+		(
+			'_label' => 'First Name',
+			'_namespace' => 'mmi',
+			'_rules' => array
+			(
+				'min_length' => array(6),
+				'alpha' => array(FALSE),
+			),
+
+			'class' => 'text',
+			'id' => 'text1',
+			'title' => 'enter a first name at least 6 characters long',
+			'value' => 'memakeit',
+		);
+		$type = 'text';
+		$txt = MMI_Form_Field::factory($type, $settings);
+
+		$form = MMI_Form::factory(array
+		(
+			'_auto_validate' => TRUE,
+			'_open' => array('_before' => 'reCAPTCHA Test Form'),
+		));
+		$form
+			->add_field($txt)
+			->add_captcha('recaptcha')
+			->add_submit()
+		;
+
+		$html = trim($form->render());
+		echo $html;
+		MMI_Debug::mdump($html, 'form', $form);
+	}
+} // End Controller_MMI_Form_Test_Form_reCAPTCHA
