331c5366b7fe0aee086dda37f30fcca2d01864d5
Shawn Sprehe <memakeit@gmail.com>
2010-10-12 13:35:52 -0600

    added filter and plugin test routes

--- init.php
+++ init.php
@@ -13,6 +13,11 @@ if (Kohana::$environment !== Kohana::PRODUCTION)
 	(
 		'directory' => 'mmi/form/test/form',
 	));
+	Route::set('mmi/form/test/filter', 'mmi/form/test/filter/<controller>(/<action>)')
+	->defaults(array
+	(
+		'directory' => 'mmi/form/test/filter',
+	));
 	Route::set('mmi/form/test/html4', 'mmi/form/test/html4/<controller>(/<action>)')
 	->defaults(array
 	(
@@ -22,5 +27,10 @@ if (Kohana::$environment !== Kohana::PRODUCTION)
 	->defaults(array
 	(
 		'directory' => 'mmi/form/test/html5',
+	));
+	Route::set('mmi/form/test/plugin', 'mmi/form/test/plugin/<controller>(/<action>)')
+	->defaults(array
+	(
+		'directory' => 'mmi/form/test/plugin',
 	));
 }
