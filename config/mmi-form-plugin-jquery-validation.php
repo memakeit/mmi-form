3a801a778080f82fc4a1e3460f3c616b099fba14
Shawn Sprehe <memakeit@gmail.com>
2010-10-15 11:31:50 -0600

    updated jquery validation config

--- config/mmi-form-plugin-jquery-validation.php
+++ config/mmi-form-plugin-jquery-validation.php
@@ -3,6 +3,11 @@
 // jQuery validation plugin configuration
 return array
 (
-	'options' => MMI_Form_Plugin_JQuery_Validation::get_default_config(TRUE, 'error', 'success'),
+	'options' => MMI_Form_Plugin_JQuery_Validation::get_default_config
+	(
+		Kohana::$environment !== Kohana::PRODUCTION,
+		'error',
+		'success'
+	),
 	'unicode' => MMI_Form_Plugin_JQuery_Validation::get_default_unicode_ranges(),
 );
