e92dbe65840670059686154e040f31425b3647a2
Shawn Sprehe <memakeit@gmail.com>
2010-10-15 21:31:12 -0600

    update views to follow coding standards

--- views/mmi/form/close.php
+++ views/mmi/form/close.php
@@ -1,8 +1,3 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-echo
-	$before.PHP_EOL.
-	'</form>'.PHP_EOL.
-	'<!-- end form -->'.PHP_EOL.
-	$after.PHP_EOL.PHP_EOL
-;
+echo $before.PHP_EOL.'</form>'.PHP_EOL.'<!-- end form -->'.PHP_EOL.$after.PHP_EOL.PHP_EOL;
