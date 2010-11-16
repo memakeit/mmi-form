a4ae3907e74a846d4020e4257be9058136e536e0
Shawn Sprehe <memakeit@gmail.com>
2010-11-16 16:44:24 -0600

    wrap the recaptcha in a div tag

--- classes/kohana/mmi/form.php
+++ classes/kohana/mmi/form.php
@@ -737,6 +737,8 @@ class Kohana_MMI_Form
 		// Configure the and add the form field
 		$options = array_merge($options, array
 		(
+			'_before'		=> '<div class="fld">'.PHP_EOL,
+			'_after'		=> '</div>'.PHP_EOL,
 			'_html'			=> array($captcha, 'html'),
 			'_source'		=> MMI_Form_Field_HTML::SRC_CALLBACK,
 			'_callbacks'	=> array
